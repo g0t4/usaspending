d35dcf52c66dbd0a33435dd07b75e0bc09b6f2bf
Wes Higbee <wes.mcclure@gmail.com>
2025-02-09 17:16:27 -0600

    exact # rows

--- queries.sql
+++ queries.sql
@@ -20,11 +20,14 @@ SELECT tablename FROM pg_tables WHERE schemaname = 'public';
 SELECT column_name, data_type, is_nullable, column_default, table_name, table_schema
 FROM information_schema.columns WHERE column_name LIKE '%recipient%name%';
 
--- estimated rows (descending, formatted with commas)
-SELECT relname AS table_name, 
-        to_char(reltuples, 'FM999,999,999,999') AS estimated_rows
-FROM pg_class
-WHERE relkind = 'r'
-ORDER BY reltuples DESC;
+-- row counts per table:
+SELECT schemaname, relname AS table_name, 
+    to_char(n_live_tup, 'FM999,999,999,999') AS exact_row_count
+FROM pg_stat_user_tables
+ORDER BY n_live_tup DESC;
 
 -- FYI <Ctrl+R> search through previous commands
+
+-- FYI pgcli has fuzzy like matching it seems:
+--   rn<Tab> => recipient_name !!!
+
