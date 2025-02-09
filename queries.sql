716ae872fdc5fc0c1c9c517cc2e9327aa4e3d9a9
Wes Higbee <wes.mcclure@gmail.com>
2025-02-09 17:12:21 -0600

    estimated rows

--- queries.sql
+++ queries.sql
@@ -17,8 +17,14 @@ SELECT tablename FROM pg_tables WHERE schemaname = 'public';
 -- \d rpt.award_search
 
 -- find columns
- SELECT column_name, data_type, is_nullable, column_default, table_name, table_schema
+SELECT column_name, data_type, is_nullable, column_default, table_name, table_schema
 FROM information_schema.columns WHERE column_name LIKE '%recipient%name%';
 
+-- estimated rows (descending, formatted with commas)
+SELECT relname AS table_name, 
+        to_char(reltuples, 'FM999,999,999,999') AS estimated_rows
+FROM pg_class
+WHERE relkind = 'r'
+ORDER BY reltuples DESC;
 
 -- FYI <Ctrl+R> search through previous commands
