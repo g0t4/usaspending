e64982f980e1b317db59e1abe67830e4bead25cd
Wes Higbee <wes.mcclure@gmail.com>
2025-02-10 11:17:28 -0600

    notes

--- queries.sql
+++ queries.sql
@@ -50,17 +50,42 @@ select * from rpt.award_search where recipient_hash = '13b50da5-5b3e-eb77-a123-7
 -- \x    -- toggle list mode (think pwsh Format-List)
 
 -- index lookup
-SELECT indexname, indexdef, tablename FROM pg_indexes
+SELECT schemaname, tablename, indexname, tablespace, indexdef
+FROM pg_indexes
 ORDER BY tablename, indexname
-SELECT 
-    indexname, 
-    pg_size_pretty(pg_relation_size(indexrelid)) AS size
+
+-- index size
+-- FYI pg_stat_user_indexes is subset of pg_stat_all_indexes https://www.postgresql.org/docs/current/monitoring-stats.html#MONITORING-PG-STAT-ALL-INDEXES-VIEW
+SELECT indexrelname, 
+    pg_size_pretty(pg_relation_size(indexrelid)) AS main_fork_size,
+    pg_size_pretty(pg_total_relation_size(indexrelid)) As total_size
 FROM pg_stat_user_indexes 
 WHERE schemaname = 'rpt' AND tablename = 'award_search';
+-- FYI pg_size_pretty(number|bigint): https://www.postgresql.org/docs/current/functions-admin.html
+select pg_size_pretty(10000000000::bigint)
 
 -- why is there an index on recipient_hash but qualified as action_date >= 2007-10-01?!
 --  152,935,236 (91+% indexed)
 --   14,342,863 (10% action_date < 2007-10-01)
+
+-- lookup pg_index joined with pg_class (to get oid)
+select I.*,C.oid 
+from pg_indexes I 
+    left join pg_class C on C.relname = I.indexname 
+where schemaname = 'rpt' and tablename = 'award_search';
+
+-- indexes by table and size
+select I.schemaname, I.tablename, I.indexname, C.oid, pg_size_pretty(pg_total_relation_size(oid)) 
+from pg_indexes I left join pg_class C on C.relname = I.indexname
+where schemaname = 'rpt' and tablename = 'award_search';
+-- OR:
+SELECT schemaname, relname, indexrelname, pg_size_pretty(pg_total_relation_size(indexrelid)) As total_size 
+FROM pg_stat_user_indexes WHERE schemaname = 'rpt' and relname = 'award_search' 
+-- include indexdef (requires using pg_indexes instead of pg_stat_user_indexes)
+select I.schemaname, I.tablename, I.indexname, C.oid, pg_size_pretty(pg_total_relation_size(oid)), I.indexdef 
+from pg_indexes I left join pg_class C on C.relname = I.indexname
+where schemaname = 'rpt' and tablename = 'award_search';
+
 
 
 
@@ -72,6 +97,9 @@ ALTER SYSTEM SET shared_buffers = '24GB'; -- 128MB default, suggested 25% RAM, u
 -- lets analyze a query first:
 EXPLAIN (ANALYZE, BUFFERS) select * from rpt.award_search where recipient_hash = '13b50da5-5b3e-eb77-a123-7daff7c433be'::UUID;
 
+
+-- TODO constraint lookup
+select oid, conname, pg_get_constraintdef(oid) from pg_constraint limit 10;
 
 /*
 operators:
