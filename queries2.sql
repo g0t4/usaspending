b685092346f2d251f5259cf2828fea8aca2b0e4f
Wes Higbee <wes.mcclure@gmail.com>
2025-02-10 13:08:57 -0600

    notes

--- queries2.sql
+++ queries2.sql
@@ -14,3 +14,31 @@ select * from pg_class where relname = 'award_search' -- => 17602
 
 -- BTW lookup class name with:
 select 17602::regclass -- => rpt.award_search
+
+-- find oid/relid for tables with 'award' in name
+select oid,relname from pg_class where relkind = 'r' and relname ilike '%award%'
+-- | 17602 | award_search                 |
+-- | 17461 | award_category               |
+-- | 17816 | subaward_search              |
+-- | 17797 | parent_award                 |
+-- | 17555 | financial_accounts_by_awards |
+
+-- full constraint query:
+select conrelid::regclass as table_from, 
+    confrelid::regclass as table_to, 
+    conname, contype,
+    pg_get_constraintdef(oid)
+from pg_constraint
+where conrelid in (select oid from pg_class where relkind = 'r' and relname ilike '%award%')
+
+-- WHERE CLAUSE REVERSE conrelid => string::regclass
+select conrelid::regclass as table_from, 
+     confrelid::regclass as table_to, 
+     conname, contype,
+     pg_get_constraintdef(oid)
+ from pg_constraint
+ where conrelid = 'rpt.subaward_search'::regclass
+-- FYI conrelid = 'rpt.subaward_search'::regclass ***  
+
+
+
