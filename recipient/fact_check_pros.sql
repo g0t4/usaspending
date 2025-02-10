5a453b067c3d08889710dce4ad7203525906b790
Wes Higbee <wes.mcclure@gmail.com>
2025-02-10 13:43:32 -0600

    ugh

--- recipient/fact_check_pros.sql
+++ recipient/fact_check_pros.sql
@@ -5,3 +5,12 @@ select * from rpt.recipient_lookup where legal_business_name ilike '%fact%check%
 -- |-------------+--------------------------------------+---------------------+--------+---------------------+---------------------+----------------------+----------+------------------------+--------------+-------------+----------------------------+-------+--------+-------+-----------------+--------+------------------------+--------+------------|
 -- | 27012781934 | 088c8a68-1683-87e4-5963-35c9bac84aeb | FACT CHECK PROS     | <null> | 81 PEARL STREET, 3B | 81 PEARL STREET, 3B | <null>               | BROOKLYN | 07                     | USA          | <null>      | <null>                     | NY    | <null> | 11201 | []              | fabs   | 2020-07-14 00:00:00+00 | <null> | <null>     |
 -- +-------------+--------------------------------------+---------------------+--------+---------------------+---------------------+----------------------+----------+------------------------+--------------+-------------+----------------------------+-------+--------+-------+-----------------+--------+------------------------+--------+------------+
+
+select * from rpt.recipient_lookup where recipient_hash = '088c8a68-1683-87e4-5963-35c9bac84aeb'::UUID
+select * from rpt.recipient_profile where recipient_hash = '088c8a68-1683-87e4-5963-35c9bac84aeb'::UUID
+
+
+-- TODO find a way to create reusable sets of queries in pgcli itself or in postgres...
+--   unfortunatley a stored procedure in postgres cannot just run queries like in MSSQL and dump results as if user had run them..
+--   have all sorts of bullshit to do the same thing... what a joke
+
